4a04a4a050243c77d12b88f5dcf03a1a44548caf
Zeniku <zjuliuscorpuz@gmail.com>
2021-04-15 09:58:39 +0800

    fix bug

--- scripts/libs/ai.js
+++ scripts/libs/ai.js
@@ -44,7 +44,7 @@ module.exports = {
 							};
 						}else if(this.target != null){
 							this.unit.lookAt(this.target);
-					    this.moveTo(this.target)
+					    this.unit.moveTo(this.target)
 							if(this.unit.inRange(this.target)){
 								this.unit.aim(this.target);
 								shoot = true;
