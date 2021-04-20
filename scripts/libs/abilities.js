e7685602540fd6b76da6b2483e85773014b2e34f
Zeniku <zjuliuscorpuz@gmail.com>
2021-04-20 15:52:38 +0800

    h

--- scripts/libs/abilities.js
+++ scripts/libs/abilities.js
@@ -1,8 +1,16 @@
+const libs = {
+	flib: require("heavymachinery/libs/function"),
+	dlib: require("heavymachinery/libs/drawlib"),
+	elib: require("heavymachinery/libs/effectlib")
+};
 function laserMoveAbility(x, y, stat, minSpeed, maxSpeed, shootSound){
   return extend(Ability, {
     update(){
       let scl = Mathf.clamp((this.unit.vel().len() - minSpeed) / (maxSpeed - minSpeed));
-      let bullet = extend(ContinuousLaserBulletType, stat);
+      let bullet = extend(ContinuousLaserBulletType, {
+        length: (Mathf.clamp(this.unit.vel().len()) * 100) * 160
+      });
+      libs.flib.merge(bullet, stat)
       let bx = this.unit.x + Angles.trnsx(this.unit.rotation, x, y)
       let by = this.unit.y + Angles.trnsy(this.unit.rotation, x, y)
       bullet.rotation(this.unit.rotation)
