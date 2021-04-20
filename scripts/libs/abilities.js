af1f34da04890250b5885079e55e3a0f87f14455
Zeniku <zjuliuscorpuz@gmail.com>
2021-04-20 15:57:07 +0800

    h

--- scripts/libs/abilities.js
+++ scripts/libs/abilities.js
@@ -5,22 +5,22 @@ const libs = {
 };
 function laserMoveAbility(x, y, stat, minSpeed, maxSpeed, shootSound){
   return extend(Ability, {
-    update(){
-      let scl = Mathf.clamp((this.unit.vel().len() - minSpeed) / (maxSpeed - minSpeed));
+    update(unit){
+      let scl = Mathf.clamp((unit.vel().len() - minSpeed) / (maxSpeed - minSpeed));
       let bullet = extend(ContinuousLaserBulletType, {
-        length: (Mathf.clamp(this.unit.vel().len()) * 100) * 160
+        length: (Mathf.clamp(unit.vel().len()) * 100) * 160
       });
       libs.flib.merge(bullet, stat)
-      let bx = this.unit.x + Angles.trnsx(this.unit.rotation, x, y)
-      let by = this.unit.y + Angles.trnsy(this.unit.rotation, x, y)
-      bullet.rotation(this.unit.rotation)
+      let bx = unit.x + Angles.trnsx(unit.rotation, x, y)
+      let by = unit.y + Angles.trnsy(unit.rotation, x, y)
+      bullet.rotation(unit.rotation)
       bullet.set(bx, by)
       if(shootSound != Sounds.none && !Vars.headless){
         if(shootSound == null) shootSound = new SoundLoop(shootSound, 1);
         shootSound.update(bx, by, true);
       }
       if(scl > minSpeed){
-      bullet.create(this.unit, this.unit.team, bx, by, this.unit.rotation)
+      bullet.create(unit, unit.team, bx, by, unit.rotation)
       }
     }
   });
