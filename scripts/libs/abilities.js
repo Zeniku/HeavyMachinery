489d405d6a362fc352b444115e6e2b444348ae2f
Zeniku <zjuliuscorpuz@gmail.com>
2021-04-25 16:56:22 +0800

    laser is yes now

--- scripts/libs/abilities.js
+++ scripts/libs/abilities.js
@@ -15,6 +15,7 @@ function laserMoveAbility(x, y, stat, speedStart, minSpeed, maxSpeed, shootSound
       let scl = Mathf.clamp((unit.vel.len() - minSpeed) / (maxSpeed - minSpeed));
       let bx = unit.x + Angles.trnsx(unit.rotation, x, y)
       let by = unit.y + Angles.trnsy(unit.rotation, x, y)
+      
       if(scl >= speedStart){
         laserbullet.create(unit, unit.team, bx, by, unit.rotation) //create the Bullet by SPEEEEEEEEED
       }
