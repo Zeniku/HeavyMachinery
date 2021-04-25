6ee51c7a3252af22f42640fcf643c5c61e544565
Zeniku <zjuliuscorpuz@gmail.com>
2021-04-25 09:14:22 +0800

    h

--- scripts/libs/abilities.js
+++ scripts/libs/abilities.js
@@ -5,7 +5,7 @@ const libs = {
 };
 function laserMoveAbility(x, y, stat, speedStart, minSpeed, maxSpeed, shootSound){
   let laserbullet = extend(ContinuousLaserBulletType, {
-    length: 5 * 8,
+    length: 5 * 8
   });
   libs.flib.merge(laserbullet, stat)
   return extend(Ability, {
@@ -13,6 +13,9 @@ function laserMoveAbility(x, y, stat, speedStart, minSpeed, maxSpeed, shootSound
       let scl = Mathf.clamp((unit.vel.len() - minSpeed) / (maxSpeed - minSpeed));
       let bx = unit.x + Angles.trnsx(unit.rotation, x, y)
       let by = unit.y + Angles.trnsy(unit.rotation, x, y)
+      if(scl >= speedStart){
+        laserbullet.create(unit, unit.team, bx, by, unit.rotation)
+      }
       if(shootSound != Sounds.none && !Vars.headless){
         let shootSoundH = null
         if(shootSound != null){
@@ -21,9 +24,6 @@ function laserMoveAbility(x, y, stat, speedStart, minSpeed, maxSpeed, shootSound
         if(shootSoundH != null){
           shootSoundH.update(bx, by, true);
         }
-      }
-      if(scl >= speedStart){
-        laserbullet.create(unit, unit.team, bx, by, unit.rotation)
       }
       libs.flib.debug("Abilities", [scl, laserbullet, shootSound, unit, unit.rotation, bx, by, Vars.headless])
     },
