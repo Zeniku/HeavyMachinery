ec436da4a3badb944fcb0158a794189eb1a870f1
Zeniku <zjuliuscorpuz@gmail.com>
2021-05-15 10:32:51 +0800

    more stuff

--- scripts/blocks/turrets.js
+++ scripts/blocks/turrets.js
@@ -5,16 +5,17 @@ let flib = require(text + "function");
 
 const overSeerBullet = blib.newOverSeerBullet({
   damage: 20,
-  speed: 2.5,
+  speed: 3.5,
   lifetime: 60,
   pierceCap: 10,
-  trailWidth: 5
+  trailWidth: 2.5
 });
 
 const overSeerTurret = extend(PowerTurret, "overSeerTurret", {
   shootType: overSeerBullet,
   buildVisibility: BuildVisibility.shown,
-  range: overSeerBullet.range()
+  range: overSeerBullet.range(),
+  recoilAmount: 2,
 });
 overSeerTurret.buildType = () => {
   return extend(PowerTurret.PowerTurretBuild, overSeerTurret, {
