87db85e429e83cd98658a7c25cae5d447fee15ba
Zeniku <zjuliuscorpuz@gmail.com>
2021-05-15 08:57:13 +0800

    stuff

--- scripts/blocks/turrets.js
+++ scripts/blocks/turrets.js
@@ -5,19 +5,24 @@ let flib = require(text + "function");
 
 const overSeerBullet = blib.newOverSeerBullet({
   damage: 20,
+  speed: 2.5,
   pierceCap: 10
 });
 
-const overSeerTurret = extend(PowerTurret, "overSeerTurret",{
-  targetPosition(pos){
-    if(!this.hasAmmo() || pos == null) return;
-
-    this.targetPos.set(pos);
-
-    if(this.targetPos.isZero()){
-      this.targetPos.set(pos);
-    }
-  },//it should fix the overseer bullet going in front of enemy and not hitting it
+const overSeerTurret = extend(PowerTurret, "overSeerTurret", {
   shootType: overSeerBullet,
   buildVisibility: BuildVisibility.shown
 });
+overSeerTurret.buildType = () => {
+  return extend(PowerTurret.PowerTurretBuild, overSeerTurret, {
+      targetPosition(pos) {
+        if(!this.hasAmmo() || pos == null) return;
+        
+        this.targetPos.set(pos)
+        
+        if(this.targetPos.isZero()) {
+          this.targetPos.set(pos);
+        }
+      },
+  });
+}
